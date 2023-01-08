079d432de4bad7caf8219aa3dc52cab9bd4f9145
yahiro <yahiro07@users.noreply.github.com>
2023-01-08 19:30:10 +0900

    add board led instances for various boards

--- examples/blink/blink.ino
+++ examples/blink/blink.ino
@@ -1,6 +1,13 @@
 #include <keyboard_peripheral_modules.h>
 
-BoardLED boardLED(25);
+BoardLED boardLED(25); //raspberry pi pico
+// BoardLED boardLED(18, 19, 20, true);      //tiny2040
+// BoardLED_NeoPixel boardLED(17, 0x40);     //kb2040
+// BoardLED_NeoPixel boardLED(12, 0x40, 11); //xiao rp2040
+// BoardLED_NeoPixel boardLED(16, 0x40);     //rp2040-zero
+// BoardLED_NeoPixel boardLED(12, 0x40, 11); //qt py rp2040
+// BoardLED_NeoPixel boardLED(25, 0x40);     //promicro rp2040
+// BoardLED_NeoPixel boardLED(23, 0x40);     //yd-rp2040
 
 void setup() {
   boardLED.initialize();
